24ef118180aca666ecd65172a244b30b638a4529
TheGames <78932550+thegameseuw@users.noreply.github.com>
2023-09-05 21:53:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   let currentCard = 0;
-  const cardElement = document.querySelector(".card");
-  const cardFrontElement = document.querySelector(".card-front");
-  const cardBackElement = document.querySelector(".card-back");
+  const cardBoxElement = document.getElementById("card-box");
 
   const flashcards = [
     { front: "Chat", back: "Cat" },
@@ -11,22 +9,39 @@ document.addEventListener("DOMContentLoaded", function() {
     // Ajoutez d'autres cartes ici
   ];
 
+  flashcards.forEach((flashcard, index) => {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+
+    const cardFrontElement = document.createElement("div");
+    cardFrontElement.classList.add("card-face", "card-front");
+    cardFrontElement.textContent = flashcard.front;
+
+    const cardBackElement = document.createElement("div");
+    cardBackElement.classList.add("card-face", "card-back");
+    cardBackElement.textContent = flashcard.back;
+
+    cardElement.appendChild(cardFrontElement);
+    cardElement.appendChild(cardBackElement);
+    cardElement.addEventListener("click", function() {
+      cardElement.classList.toggle("flipped");
+    });
+    
+    cardBoxElement.appendChild(cardElement);
+  });
+
   function showNextCard() {
-    if (currentCard < flashcards.length) {
-      cardFrontElement.textContent = flashcards[currentCard].front;
-      cardBackElement.textContent = flashcards[currentCard].back;
-      currentCard++;
-    } else {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => card.classList.remove('active'));
+    cards[currentCard].classList.add('active');
+
+    currentCard++;
+    if (currentCard >= flashcards.length) {
       currentCard = 0;
     }
   }
 
-  cardElement.addEventListener("click", function() {
-    cardElement.classList.toggle("flipped");
-  });
-
   document.getElementById("next-card").addEventListener("click", function() {
-    cardElement.classList.remove("flipped");
     showNextCard();
   });
 
